80c0029c2e666354f1edce03e37bdeabf9b5b5e2
Aaron <72924198+AaronCrvl@users.noreply.github.com>
2021-10-18 17:31:06 -0300

    Usage of Bootstrap
    - Install and reference of Bootstrap elements
    - Insertion of dropdowns and inputs
    - Adjustments in CSS

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from 'react';
 import './css/Header.css';
 
 //class Function
-const Header = () =>       
-<div id="Header">
+const Header = () =>    
+<div id="Header">    
+    <div class="btn-extra" type="button">
+        <button id="GitHubSlides">
+            Show me some Projects
+        </button>
+    </div>
+    <div class="dropdown">
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="true">
+    Main Options
+    </button>
+        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+            <li><a class="dropdown-item" href="#">Reference 1</a></li>
+            <li><a class="dropdown-item" href="#">Reference 2</a></li>
+            <li><a class="dropdown-item" href="#">Reference 3</a></li>
+        </ul>
+    </div>
+    
+    <div class="dropdown">
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="true">
+    Tutorials
+    </button>
+        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+            <li><a class="dropdown-item" href="#">Reference 1</a></li>
+            <li><a class="dropdown-item" href="#">Reference 2</a></li>
+            <li><a class="dropdown-item" href="#">Reference 3</a></li>
+        </ul>
+    </div>
+    <div class="dropdown">
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="true">
+    About
+    </button>
+        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+            <li><a class="dropdown-item" href="#">Reference 1</a></li>
+            <li><a class="dropdown-item" href="#">Reference 2</a></li>
+            <li><a class="dropdown-item" href="#">Reference 3</a></li>
+        </ul>
+    </div>
+</div>
+
+{/* <div id="Header">
     <table class="Table">
         <tr>
             <th>
@@ -17,6 +56,6 @@ const Header = () =>
             </th>
         </tr>        
     </table>
-</div>
+</div> */}
 
 export default Header;
--- src/components/WebPage.js
+++ src/components/WebPage.js
@@ -3,12 +3,28 @@ import React from 'react'
 
 //class Function
 const WebPage = () =>       
-<div>    
+<div class="Entry">    
     <p>
-        <input id="Area" type="email" name="test@example.com.br"></input>          
-        <button id="Button"></button>
-        <br></br>         
-        <a href="" class="Link">Learn More</a>                
+        <img src="https://media.giphy.com/media/13twUEuUnCrEju/giphy.gif" width="20%"></img>
+    </p>
+    <h1>Log Into Atomic Network</h1>
+    <br></br>
+    <p>
+        <form id="DataEntry">
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"></input>
+            </div>
+            <div class="form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>        
+                <label for="exampleInputEmail1">Select if you not a robot</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
     </p>
 </div>
 
